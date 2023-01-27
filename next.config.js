255a8ec09904a5bd34b6c698e82761361bce9091
Illia Krytskyi <I.Krytskiy@evnedev.com>
2023-01-27 21:28:24 +0200

    update next config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    loader: 'static',
+  },
   experimental: {
     appDir: true,
-    images: {
-      unoptimized: true,
-    },
   },
 };
 
