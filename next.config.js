bddb9641d109ddc6c89f6f64eeab72e9bf327af3
Illia Krytskyi <I.Krytskiy@evnedev.com>
2023-06-03 14:26:53 +0300

    update components

--- next.config.js
+++ next.config.js
@@ -1,38 +1,6 @@
 /** @type {import('next').NextConfig} */
-// const isGithubActions = process.env.GITHUB_ACTIONS || false;
-
-// let assetPrefix = '';
-// let basePath = '/';
-//
-// if (isGithubActions) {
-//   // trim off `<owner>/`
-//   const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
-//
-//   assetPrefix = `/${repo}/`;
-//   basePath = `/${repo}`;
-// }
 const pathPrefix = process.env.NODE_ENV === 'production' ? '/waxing' : '';
 const nextConfig = {
-  // assetPrefix: pathPrefix,
-  // assetPrefix: '',
-  // basePath: pathPrefix,
-  // basePath: 'https://scarlettjohanssons.github.io/waxing/',
-  // images: {
-  //   loader: 'imgix',
-  //   path: 'https://scarlettjohanssons.github.io/waxing/',
-  // },
-  // images: {
-  //   loader: 'default',
-  //   unoptimized: true,
-  //   remotePatterns: [
-  //     {
-  //       protocol: 'https',
-  //       hostname: 'scarlettjohanssons.github.io',
-  //       port: '',
-  //       pathname: '/waxing/**',
-  //     },
-  //   ],
-  // },
   experimental: {
     appDir: true,
   },
